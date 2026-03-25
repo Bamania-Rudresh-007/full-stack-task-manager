dec8d9462fec8c14b3aa0f1e5142097bc2c8e7f3
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-25 13:52:50 +0530

    update backend link

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "http://localhost:7000/api"});
+const API = axios.create({baseURL: "https://full-stack-task-manager-hzhr.onrender.com/api"});
 
 API.interceptors.request.use((req) => {
     const token = JSON.parse(localStorage.getItem("token"));
